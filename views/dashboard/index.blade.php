d957b7f186a3214b959a677fe77f9fae86c17e38
deanFL0 <119829228+deanFL0@users.noreply.github.com>
2023-05-15 19:46:29 +0700

    Add files via upload

---
+++ views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('master')
+
+@section('content')
+<h1>hello</h1>
+@endsection
---
+++ views/login/index.blade.php
@@ -0,0 +1,61 @@
+<head>
+    <title>
+        Login
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+    </script>
+</head>
+<section class="vh-100">
+    @if (session()->has('success'))
+        <div class="alert alert-success text-center" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
+    @if (session()->has('error'))
+        <div class="alert alert-danger text-center" role="alert">
+            {{ session('error') }}
+        </div>
+    @endif
+    <div class="container py-5 h-100">
+        <div class="row d-flex align-items-center justify-content-center h-100">
+            <div class="col-md-8 col-lg-7 col-xl-6">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                    class="img-fluid" alt="Phone image">
+            </div>
+            <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+
+                <form method="POST" action="/login">
+                    @csrf
+                    <!-- Username input -->
+                    <div class="form-floating">
+                        <input type="text" class="form-control" name="username" id="username" placeholder="Username"
+                            autofocus required>
+                        <label for="username">Username</label>
+                    </div>
+
+                    <!-- Password input -->
+                    <div class="form-floating">
+                        <input type="password" class="form-control" name="password" id="password"
+                            placeholder="Password" required>
+                        <label for="password">Password</label>
+                    </div>
+
+                    {{-- <div class="d-flex justify-content-around align-items-center mb-4">
+                        <!-- Checkbox -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+                            <label class="form-check-label" for="form1Example3"> Remember me </label>
+                        </div>
+                        <a href="#!">Forgot password?</a>
+                    </div> --}}
+
+                    <!-- Submit button -->
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">Log in</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
