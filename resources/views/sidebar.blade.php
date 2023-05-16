7e1187c1d084adc84cc5af5b41ff8133cc67ab42
deanFL0 <dean.fl20@student.unibi.ac.id>
2023-05-16 09:33:02 +0700

    change logo

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #135b71;">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
-        <img src="{{ asset('adminlte') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+        <img src="{{ asset('adminlte') }}/dist/img/pgri.png" alt="Logo PGRI"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
