c13a61cb2c56f69b6dd81351e8959d4b5bb8e132
deanFL0 <dean.fl20@student.unibi.ac.id>
2023-05-22 09:31:39 +0700

    fix login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,11 +12,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="{{ asset('adminlte')}}/dist/css/login.css">
+    <link rel="stylesheet" href="{{ asset('adminlte') }}/dist/css/login.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<section class="vh-100">
+
+<body>
     @if (session()->has('success'))
         <div class="alert alert-success text-center" role="alert">
             {{ session('success') }}
@@ -27,31 +28,36 @@
             {{ session('error') }}
         </div>
     @endif
-    <div class="container-fluid h-100">
-        <div class="row h-100 align-items-center justify-content-center">
-            <div class="col-md-7 col-xs-7 mb-5">
-                <img src="{{ asset('adminlte')}}/dist/img/login.png" class="img-fluid" alt="Phone image">
-            </div>
-            <div class="col-md-5 col-xs-6">
+    <section class="vh-100">
+        <div class="container-fluid py-5 h-100">
+            <div class="row h-100 align-items-center justify-content-center">
+                <div class="col-md-7 col-xs-7 mb-5">
+                    <img src="{{ asset('adminlte') }}/dist/img/login.png" class="img-fluid" alt="Phone image">
+                </div>
+                <div class="col-md-5 col-xs-6">
 
-                <form method="POST" action="/login" class="needs-validation" novalidate>
-                    <!-- Username input -->
-                    <div class="form-floating mb-4">
-                        <input type="text" class="form-control form-control-lg" name="username" id="username" placeholder="Username" autofocus required>
-                        <label for="username">Username</label>
-                    </div>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <!-- Username input -->
+                        <div class="form-floating mb-4">
+                            <input type="text" class="form-control form-control-lg" name="username" id="username"
+                                placeholder="Username" autofocus required>
+                            <label for="username">Username</label>
+                        </div>
 
-                    <!-- Password input -->
-                    <div class="form-floating mb-4">
-                        <input type="password" class="form-control form-control-lg" name="password" id="password" placeholder="Password" required>
-                        <label for="password">Password</label>
-                    </div>
+                        <!-- Password input -->
+                        <div class="form-floating mb-4">
+                            <input type="password" class="form-control form-control-lg" name="password" id="password"
+                                placeholder="Password" required>
+                            <label for="password">Password</label>
+                        </div>
 
-                    <!-- Submit button -->
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">Log in</button>
-                </form>
+                        <!-- Submit button -->
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">Log in</button>
+                    </form>
 
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+</body>
