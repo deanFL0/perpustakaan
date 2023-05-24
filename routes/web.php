cf47567508d03504528c3748f13ae83d5c8cd969
deanFL0 <dean.fl20@student.unibi.ac.id>
2023-05-24 09:38:55 +0700

    revert back last commit

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ Route::group(['middleware' => ['auth', 'checkrole:admin']], function () {
     Route::get('/user', [UserController::class, 'index'])->name('user');
     Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
     Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+    Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::post('/user/update', [UserController::class, 'update'])->name('user.update');
     Route::delete('/user/destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 });
-Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user.edit')->middleware('auth');
-Route::post('/user/update', [UserController::class, 'update'])->name('user.update')->middleware('auth');
